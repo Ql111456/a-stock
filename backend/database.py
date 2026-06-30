69ad5cc456dc459728d003ecae20e13993712b1b
Ql111456 <925958049@qq.com>
2026-06-30 13:51:10 +0800

    add backend/database.py

---
+++ backend/database.py
@@ -0,0 +1,12 @@
+"""SQLite 数据库设置"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DB_PATH = os.path.join(os.path.dirname(__file__), "stock.db")
+engine = create_engine(f"sqlite:///{DB_PATH}", connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
